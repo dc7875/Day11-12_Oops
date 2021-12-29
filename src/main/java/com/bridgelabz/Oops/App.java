d05d93120f11393b329814ab1e8a79a8b516550d
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-29 15:30:29 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/Oops/App.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.Oops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("welcome to oops program");
+    }
+}
